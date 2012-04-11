046538cc231c3a2f7f525b256a07488badde000f
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-04-11 04:13:46 +0000

    Change NAV

--- trunk/omg/sale.py
+++ trunk/omg/sale.py
@@ -447,6 +447,61 @@ class sale_order(osv.osv):
             current = 0
         return current
 
+    def _make_invoice(self, cr, uid, order, lines, context=None):
+        journal_obj = self.pool.get('account.journal')
+        inv_obj = self.pool.get('account.invoice')
+        obj_invoice_line = self.pool.get('account.invoice.line')
+        if context is None:
+            context = {}
+
+        journal_ids = journal_obj.search(cr, uid, [('type', '=', 'sale'), ('company_id', '=', order.company_id.id)], limit=1)
+        if not journal_ids:
+            raise osv.except_osv(_('Error !'),
+                _('There is no sales journal defined for this company: "%s" (id:%d)') % (order.company_id.name, order.company_id.id))
+        a = order.partner_id.property_account_receivable.id
+        pay_term = order.payment_term and order.payment_term.id or False
+        invoiced_sale_line_ids = self.pool.get('sale.order.line').search(cr, uid, [('order_id', '=', order.id), ('invoiced', '=', True)], context=context)
+        from_line_invoice_ids = []
+        for invoiced_sale_line_id in self.pool.get('sale.order.line').browse(cr, uid, invoiced_sale_line_ids, context=context):
+            for invoice_line_id in invoiced_sale_line_id.invoice_lines:
+                if invoice_line_id.invoice_id.id not in from_line_invoice_ids:
+                    from_line_invoice_ids.append(invoice_line_id.invoice_id.id)
+        for preinv in order.invoice_ids:
+            if preinv.state not in ('cancel',) and preinv.id not in from_line_invoice_ids:
+                for preline in preinv.invoice_line:
+                    inv_line_id = obj_invoice_line.copy(cr, uid, preline.id, {'invoice_id': False, 'price_unit': -preline.price_unit})
+                    lines.append(inv_line_id)
+        currency_ids = self.pool.get('res.currency').search(cr, uid, [('name','=','THB')])
+        if currency_ids:
+            currency_obj = self.pool.get('res.currency').browse(cr, uid, currency_ids)[0]
+        currency_id = currency_obj.id
+        inv = {
+            'name': order.client_order_ref or '',
+            'origin': order.name,
+            'type': 'out_invoice',
+            'reference': order.client_order_ref or order.name,
+            'account_id': a,
+            'partner_id': order.partner_id.id,
+            'journal_id': journal_ids[0],
+            'address_invoice_id': order.partner_invoice_id.id,
+            'address_contact_id': order.partner_order_id.id,
+            'invoice_line': [(6, 0, lines)],
+            'currency_id': currency_id,
+            'comment': order.note,
+            'payment_term': pay_term,
+            'fiscal_position': order.fiscal_position.id or order.partner_id.property_account_position.id,
+            'date_invoice': time.strftime('%Y-%m-%d'),
+            'company_id': order.company_id.id,
+            'user_id': order.user_id and order.user_id.id or False
+        }
+        inv.update(self._inv_get(cr, uid, order))
+        inv_id = inv_obj.create(cr, uid, inv, context=context)
+        data = inv_obj.onchange_payment_term_date_invoice(cr, uid, [inv_id], pay_term, time.strftime('%Y-%m-%d'))
+        if data.get('value', False):
+            inv_obj.write(cr, uid, [inv_id], data['value'], context=context)
+        inv_obj.button_compute(cr, uid, [inv_id])
+        return inv_id
+
     #POP-015
     def action_invoice_create(self, cr, uid, ids, grouped=False, states=['confirmed', 'done', 'exception'], date_inv = False, context=None):
         res = False
