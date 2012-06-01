ffe307b2b026f7074373348e2c49491efe93b977
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-01 08:34:52 +0000

    Change Bug TS in Export FOS

--- ineco_export_fos/sale.py
+++ ineco_export_fos/sale.py
@@ -367,27 +367,55 @@ class sale_order(osv.osv):
                     raise osv.except_osv(_('Error !'), _('Please config FOS Server in company.'))
 
             contr_detailts = """
+
                 select
-                  osc.name as chaincd, --'wait change - osrc.chain_id' as chaincd,
-                  so.name as contractno,
-                  sl.store_code as storecd,
-                  oslg.name as groupcd,
-                  '0' as flagchkts,
-                  'TS' as typets,
-                  1 as noofts
-                from sale_order so
-                join sale_branch_line sbl on sbl.sale_id = so.id
-                join stock_location sl on sbl.location_id = sl.id
-                join omg_sale_location_group oslg on sl.location_group_id = oslg.id
-                 join omg_sale_reserve_contact osrc on so.client_order_ref = osrc.name
-                 join omg_sale_period osp on so.period_id = osp.id
-                left join product_category pc on so.service_category_id = pc.id
-                 join omg_sale_chain osc on osrc.chain_id = osc.id
-                 join res_users ru3 on so.create_uid = ru3.id
-                 join omg_sale_period_category ospc on ospc.id = osp.category_id
-                 left join omg_sale_location_type oslt on sl.location_type_id = oslt.id 
-                where so.company_id = %s and so.id = %s 
-            """
+                      osc.name as chaincd, --'wait change - osrc.chain_id' as chaincd,
+                      so.name as contractno,
+                      sl.store_code as storecd,
+                      oslg.name as groupcd,
+                      '0' as flagchkts,
+                      coalesce((select pp.default_code from sale_order  so
+                    join sale_order_line sol on so.id = sol.order_id
+                    join product_product pp on sol.product_id = pp.id
+                    where sol.product_id in (select id from product_product 
+                where default_code in ('TS', 'MC', 'SP', 'Leader', 'Pretty Girl', 'Staff', 'OT', 'PT'))
+                and so.id = %s limit 1),'TS') as typets,
+                      --'TS' as typets,
+                     1 as noofts
+                    from sale_order so
+                    left join sale_branch_line sbl on sbl.sale_id = so.id
+                    left join stock_location sl on sbl.location_id = sl.id
+                    left join omg_sale_location_group oslg on sl.location_group_id = oslg.id
+                     left join omg_sale_reserve_contact osrc on so.client_order_ref = osrc.name
+                     join omg_sale_period osp on so.period_id = osp.id
+                    left join product_category pc on so.service_category_id = pc.id
+                     left join omg_sale_chain osc on osrc.chain_id = osc.id
+                     join res_users ru3 on so.create_uid = ru3.id
+                     left join omg_sale_period_category ospc on ospc.id = osp.category_id
+                     left join omg_sale_location_type oslt on sl.location_type_id = oslt.id 
+                    where so.company_id = %s and so.id = %s            """
+
+#                select
+#                  osc.name as chaincd, --'wait change - osrc.chain_id' as chaincd,
+#                  so.name as contractno,
+#                  sl.store_code as storecd,
+#                  oslg.name as groupcd,
+#                  '0' as flagchkts,
+#                  'TS' as typets,
+#                  1 as noofts
+#                from sale_order so
+#                join sale_branch_line sbl on sbl.sale_id = so.id
+#                join stock_location sl on sbl.location_id = sl.id
+#                join omg_sale_location_group oslg on sl.location_group_id = oslg.id
+#                 join omg_sale_reserve_contact osrc on so.client_order_ref = osrc.name
+#                 join omg_sale_period osp on so.period_id = osp.id
+#                left join product_category pc on so.service_category_id = pc.id
+#                 join omg_sale_chain osc on osrc.chain_id = osc.id
+#                 join res_users ru3 on so.create_uid = ru3.id
+#                 join omg_sale_period_category ospc on ospc.id = osp.category_id
+#                 left join omg_sale_location_type oslt on sl.location_type_id = oslt.id 
+#                where so.company_id = %s and so.id = %s 
+#            """
             cr.execute(contr_detailts % (order.company_id.id, order.id))
             line_data =  cr.dictfetchall()
             for data in line_data:
@@ -422,34 +450,28 @@ class sale_order(osv.osv):
 
 
             costitem_sql = """
-                select
-                      osc.name as chaincd, --'wait change - osrc.chain_id' as chaincd,
-                      so.name as contractno,
-                      sl.store_code as storecd,
-                      oslg.name as groupcd,
-                      '0' as flagchkts,
-                      coalesce((select pp.default_code from sale_order  so
-                    join sale_order_line sol on so.id = sol.order_id
-                    join product_product pp on sol.product_id = pp.id
-                    where sol.product_id in (select id from product_product 
-                where default_code in ('TS', 'MC', 'SP', 'Leader', 'Pretty Girl', 'Staff', 'OT', 'PT'))
-                and so.id = %s limit 1),'TS') as typets,
-                      --'TS' as typets,
-                      1 as noofts
-                    from sale_order so
-                    left join sale_branch_line sbl on sbl.sale_id = so.id
-                    left join stock_location sl on sbl.location_id = sl.id
-                    left join omg_sale_location_group oslg on sl.location_group_id = oslg.id
-                     left join omg_sale_reserve_contact osrc on so.client_order_ref = osrc.name
-                     join omg_sale_period osp on so.period_id = osp.id
-                    left join product_category pc on so.service_category_id = pc.id
-                     left join omg_sale_chain osc on osrc.chain_id = osc.id
-                     join res_users ru3 on so.create_uid = ru3.id
-                     left join omg_sale_period_category ospc on ospc.id = osp.category_id
-                     left join omg_sale_location_type oslt on sl.location_type_id = oslt.id 
-                    where so.company_id = %s and so.id = %s            """
-            cr.execute(costitem_sql % (order.id, order.company_id.id, order.id))
-
+                so.name as contractno,
+                so.client_order_ref as bookingno,
+                pp.id as costitem,
+                1 as lineseq,
+                pc.name as typecd,
+                pp.name_template as costdesc,
+                product_uom_qty as chargeqty,
+                'Day' as chargeuom,
+                (select count(*) from sale_branch_line where sale_id = so.id) as altqty ,
+                'Store' as altuom,
+                coalesce(price_unit,0) as chargerate,
+                  product_uom_qty * coalesce((select count(*) from sale_branch_line where sale_id = so.id),1) * coalesce(price_unit,1)  as extcharge
+                from
+                sale_order so
+                join sale_order_line sol on so.id = sol.order_id
+                left join omg_sale_reserve_contact osrc on so.client_order_ref = osrc.name
+                left join product_product pp on sol.product_id = pp.id
+                left join product_template pt on pp.product_tmpl_id = pt.id
+                left join product_category pc on pt.categ_id = pc.id
+                where so.company_id = %s and so.id = %s
+                """
+            cr.execute(costitem_sql % (order.company_id.id, order.id))
             line_data =  cr.dictfetchall()
             for data in line_data:
                 insert_field = self._genfield(data,1);
@@ -469,12 +491,10 @@ class sale_order(osv.osv):
                         #insert_sql
                         cur.execute('SET ANSI_WARNINGS off')
                         cur.execute(insert_contrmf_sql.encode('utf-8'))
-
                     except:
                         #update_sql
                         cur.execute('SET ANSI_WARNINGS off')
-                       # cur.execute(update_contrmf_sql.encode('utf-8'), (data['contractno'], data['costitem'],))
-
+                        cur.execute(update_contrmf_sql.encode('utf-8'), (data['contractno'], data['costitem'],))
                     cur.close()
                     conn.commit()
                     
