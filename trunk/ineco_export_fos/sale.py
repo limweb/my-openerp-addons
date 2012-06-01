2159d8ac57c1741156bbc2f7b6c432ade61d6480
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-01 10:12:06 +0000

    change bug fos

--- trunk/ineco_export_fos/sale.py
+++ trunk/ineco_export_fos/sale.py
@@ -278,16 +278,16 @@ class sale_order(osv.osv):
                     conn = pymssql.connect(host=server_ip, user=server_user, password=server_password, 
                                            database=server_db,as_dict=True)
                     cur = conn.cursor()
-                    #try:
+                    try:
                         #insert_sql
-                    cur.execute('SET ANSI_WARNINGS off')
-                    raise osv.except_osv(_('Error !'), _(insert_contrmf_sql))
-                    cur.execute(insert_contrmf_sql.encode('utf-8'))
+                        cur.execute('SET ANSI_WARNINGS off')
+                    #raise osv.except_osv(_('Error !'), _(insert_contrmf_sql))
+                        cur.execute(insert_contrmf_sql.encode('utf-8'))
 
-                    #except:
+                    except:
                         #update_sql
-                    #    cur.execute('SET ANSI_WARNINGS off')
-                    #    cur.execute(update_contrmf_sql.encode('utf-8'))
+                        cur.execute('SET ANSI_WARNINGS off')
+                        cur.execute(update_contrmf_sql.encode('utf-8'))
 
                     cur.close()
                     conn.commit()
