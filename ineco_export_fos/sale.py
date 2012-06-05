8f2e31da6ddb4fb302f9200c86c6eab1436c9570
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-05 06:09:42 +0000

    Change Bug contr_prod

--- ineco_export_fos/sale.py
+++ ineco_export_fos/sale.py
@@ -87,14 +87,14 @@ class sale_order(osv.osv):
             #POP-002
             sql_clear_prod = """
                 update contr_prod 
-                set itemno1 = null, itemdesc1=null, barcodeno1=null,
-                    itemno2 = null, itemdesc2=null, barcodeno2=null,
-                    itemno3 = null, itemdesc3=null, barcodeno3=null,
-                    itemno4 = null, itemdesc4=null, barcodeno4=null,
-                    itemno5 = null, itemdesc5=null, barcodeno5=null,
-                    itemno6 = null, itemdesc6=null, barcodeno6=null,
-                    itemno7 = null, itemdesc7=null, barcodeno7=null,
-                    itemno8 = null, itemdesc8=null, barcodeno8=null
+                set itemno1 = '', itemdesc1=null, barcodeno1=null,
+                    itemno2 = '', itemdesc2=null, barcodeno2=null,
+                    itemno3 = '', itemdesc3=null, barcodeno3=null,
+                    itemno4 = '', itemdesc4=null, barcodeno4=null,
+                    itemno5 = '', itemdesc5=null, barcodeno5=null,
+                    itemno6 = '', itemdesc6=null, barcodeno6=null,
+                    itemno7 = '', itemdesc7=null, barcodeno7=null,
+                    itemno8 = '', itemdesc8=null, barcodeno8=null
                 where 
                     bookingno = '%s' and contractno= '%s' 
                     
