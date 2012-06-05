3ef3deb6c4bd4dc4b9ec4a40b59311946575b219
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-05 04:39:19 +0000

    OK

--- trunk/ineco_base/__openerp__.py
+++ trunk/ineco_base/__openerp__.py
@@ -47,6 +47,7 @@
         "wizard/wizard_schedule_delivery_compute_view.xml",
         "report_data.xml",
         "wizard/wizard_stock_report_print_view.xml",
+        "wizard/wizard_export_stock_counting_view.xml",
          ],
     'demo_xml': [],
     'installable': True,
