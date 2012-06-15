1e88ec88b5bbe6a4502d0d1da2f2a4da86380b38
thitithup <thitithup@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-15 01:02:12 +0000

    Change TCG

--- trunk/tcg/product.py
+++ trunk/tcg/product.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,17 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-import purchase
-import purchase_requisition_partner
-import sale
-import purchase_requisition
-import mrp
-import product
+from datetime import datetime
+from osv import osv, fields
+from tools.translate import _
+import netsvc
+import time
+import tools
+import decimal_precision as dp
 
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
+class product_product(osv.osv):
+    
+    _name = 'product.product'
+    _inherit = 'product.product'
+    _description = 'Add Stock Journal In Product.Product'
+    _columns = {
+        'ineco_stock_journal_id': fields.many2one('stock.journal','Stock Journal'),
+    }
+    
+product_product()
