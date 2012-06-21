4eb920136257436b56d6b5614960aad5171e0979
thitithup@gmail.com <thitithup@gmail.com@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-21 12:01:11 +0000

    Change

--- trunk/ineco_export_fos/partner.py
+++ trunk/ineco_export_fos/partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,13 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-import partner
-import company
-import sale 
+from datetime import datetime
+from osv import osv, fields
+from tools.translate import _
+import netsvc
+import time
+import tools
+import decimal_precision as dp
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
+class res_partner(osv.osv):
+    
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    _description = 'Add Fos Code In Product.Product'
+    _columns = {
+        'ineco_fos_code': fields.char('FOS Code', size=20),
+    }
+    
+res_partner()
