1bd1be9828c4b70a1ba486a1357a5d19d3f5ba73
thitithup@gmail.com <thitithup@gmail.com@63ab4253-1d0f-0ef1-39c5-73446a8bc111>
2012-06-30 09:10:39 +0000

    OK

--- tcg/product.py
+++ tcg/product.py
@@ -37,3 +37,24 @@ class product_product(osv.osv):
     }
     
 product_product()
+
+class product_uom(osv.osv):
+    
+    _name = 'product.uom'
+    _inherit = 'product.uom'
+    _description = 'change uom warning'
+    
+    def _compute_qty_obj(self, cr, uid, from_unit, qty, to_unit, context=None):
+        if context is None:
+            context = {}
+        if from_unit.category_id.id <> to_unit.category_id.id:
+            #if context.get('raise-exception', True):
+            #    raise osv.except_osv(_('Error !'), _('Conversion from Product UoM %s to Default UoM %s is not possible as they both belong to different Category!.') % (from_unit.name,to_unit.name,))
+            #else:
+            return qty
+        amount = qty / from_unit.factor
+        if to_unit:
+            amount = rounding(amount * to_unit.factor, to_unit.rounding)
+        return amount
+    
+product_uom()    
